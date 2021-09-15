4ba776c4fcf34d950d10c7e9f395ab7bf3aa1579
Namgyu Ho <itsnamgyu@gmail.com>
2021-09-16 01:29:30 +0900

    Use common dataset paths

--- configs.py
+++ configs.py
@@ -1,10 +1,10 @@
 save_dir                    = './logs'
 
-miniImageNet_path           	= '/home/osilab7/hdd/cdfsl/miniImagenet'
-miniImageNet_test_path          = '/home/osilab7/hdd/cdfsl/miniImagenet'
+miniImageNet_path           	= '/home/data/cdfsl/miniImagenet'
+miniImageNet_test_path          = '/home/data/cdfsl/miniImagenet'
 DTD_path 						= '/ssd/dtd/images/'
 
-ISIC_path = "/home/osilab7/hdd/cdfsl/ISIC"
-ChestX_path = "/home/osilab7/hdd/cdfsl/chestX"
-CropDisease_path = "/home/osilab7/hdd/cdfsl/CropDiseases"
-EuroSAT_path = "/home/osilab7/hdd/cdfsl/EuroSAT"
+ISIC_path = "/data/cdfsl/ISIC"
+ChestX_path = "/data/cdfsl/chestX"
+CropDisease_path = "/data/cdfsl/CropDiseases"
+EuroSAT_path = "/data/cdfsl/EuroSAT"
