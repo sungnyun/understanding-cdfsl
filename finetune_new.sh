53f3ec65ba8e7b65dbf3a9742e691f2bf1c1094a
Namgyu Ho <itsnamgyu@gmail.com>
2021-12-28 18:21:24 +0900

    [Minor] Add miniImageNet_test target example in shell scripts

--- finetune_new.sh
+++ finetune_new.sh
@@ -1,24 +1,24 @@
 ## Type 1
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./finetune_new.py --ls --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr  --ft_parts head --split_seed 1
 done
 
 ## Type 2
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./finetune_new.py --us --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr  --ft_parts head --split_seed 1
 done
 
 ## Type 3
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./finetune_new.py ---ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr  --ft_parts head --split_seed 1
 done
 
 ## Type 4
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./finetune_new.py --ls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr  --ft_parts head --split_seed 1
 done
 
 ## Type 5
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./finetune_new.py --us --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr  --ft_parts head --split_seed 1
 done
--- pretrain_new.sh
+++ pretrain_new.sh
@@ -5,16 +5,16 @@ python ./pretrain_new.py --ls --source_dataset miniImageNet --backbone resnet10 
 python ./pretrain_new.py --us --source_dataset miniImageNet --backbone resnet10 --model simclr --epochs 200
 
 ## Type 3
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./pretrain_new.py --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
 
 ## Type 4
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./pretrain_new.py --ls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
 
 # Type 5
-for TARGET in "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
+for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./pretrain_new.py --us --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
