73c692f250f0b4a416129a451a1b47b889905254
Namgyu Ho <itsnamgyu@gmail.com>
2022-01-09 01:49:49 +0900

    Update type # in pretrain_new.sh to reflect paper

--- pretrain_new.sh
+++ pretrain_new.sh
@@ -2,32 +2,22 @@
 python ./pretrain_new.py --ls --source_dataset miniImageNet --backbone resnet10 --model simclr --epochs 200
 
 ## Type 2
-python ./pretrain_new.py --us --source_dataset miniImageNet --backbone resnet10 --model simclr --epochs 200
+python ./pretrain_new.py --ut --source_dataset miniImageNet --backbone resnet10 --model simclr --epochs 200
 
 ## Type 3
 for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
-  python ./pretrain_new.py --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
-done
-
-## Type 4
-for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
   python ./pretrain_new.py --ls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
-done
-
-# Type 5
-for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
-  python ./pretrain_new.py --us --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
 
 ## PLS pretrain (note 1, should use --model base for ls pretrain) (note 2, should use same --tag for ls and pls)
 python ./pretrain_new.py --ls --source_dataset miniImageNet --backbone resnet10 --model base
 
-## Type 6
+## Type 4
 for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
-  python ./pretrain_new.py --pls --ls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
+  python ./pretrain_new.py --pls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
 
-# Type 7
+# Type 5
 for TARGET in "miniImageNet_test" "CropDisease" "EuroSAT" "ISIC" "ChestX"; do
-  python ./pretrain_new.py --pls --us --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
+  python ./pretrain_new.py --pls --ls --ut --source_dataset miniImageNet --target_dataset $TARGET --backbone resnet10 --model simclr
 done
