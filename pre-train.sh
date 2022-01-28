90a89bf85ca39a3c3a0ddb26b373daf6e6041e75
Namgyu Ho <itsnamgyu@gmail.com>
2022-01-28 20:11:18 +0900

    Remove sh and ipynb files

--- pre-train.sh
+++
@@ -1,6 +0,0 @@
-### Pre-train
-#python ./pretrain_simsiam.py --dataset miniImageNet --model ResNet10 --method baseline --track_bn --pretrain_type 1 --aug_mode base
-python ./pretrain_simsiam.py --dataset miniImageNet --model ResNet10 --method baseline --track_bn --pretrain_type 3 --dataset_names CropDisease --aug_mode base
-python ./pretrain_simsiam.py --dataset miniImageNet --model ResNet10 --method baseline --track_bn --pretrain_type 4 --dataset_names CropDisease --aug_mode base
-python ./pretrain_simsiam.py --dataset miniImageNet --model ResNet10 --method baseline --track_bn --pretrain_type 5 --dataset_names CropDisease --aug_mode base
-python ./pretrain_simsiam.py --dataset miniImageNet --model ResNet10 --method baseline --track_bn --pretrain_type 2 --aug_mode base
