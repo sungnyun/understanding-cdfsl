3b3b7fb7978fd3edb777e7518fbb9a511b0650c8
Namgyu Ho <itsnamgyu@gmail.com>
2022-03-02 23:37:21 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CD-FSL',
+    version='',
+    packages=['data', 'model', 'methods', 'datasets'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
